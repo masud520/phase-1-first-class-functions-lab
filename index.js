b7f0a04de9c342c80a534595745b751ce7d156b8
masud520 <masud.abdi@student.moringaschool.com>
2023-10-07 23:05:33 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers
+  const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+  };
+  
